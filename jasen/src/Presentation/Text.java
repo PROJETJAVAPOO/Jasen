ed485ddd80df078275d02224393bf7d3bd254413
NaaviX <antoine.legrand77@gmail.com>
2018-02-05 20:41:58 +0100

    Composant presentation documents

--- jasen/src/Presentation/Text.java
+++ jasen/src/Presentation/Text.java
@@ -1,1 +1,14 @@
+import java.util.*;
 
+public class Text {
+	String path;
+	String presentation;
+	
+	public Text(String path) {
+		this.path = path;
+		List<String> tmp = parsefiletoindex(path);
+		this.presentation = tmp.get(1)+" "+tmp.get(2)+ "...";
+	}
+	
+	
+}
